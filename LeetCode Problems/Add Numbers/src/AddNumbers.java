d23ec03116843ebd9408d67504a372b90b930d5d
arnabmaity <arnmaity16@gmail.com>
2021-11-20 17:28:35 +0530

    Add Numbers

--- LeetCode Problems/Add Numbers/src/AddNumbers.java
+++ LeetCode Problems/Add Numbers/src/AddNumbers.java
@@ -1,2 +1,81 @@
+
+
+//  Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(-1);
+        ListNode l3 = result;
+        int nextCarry = 0, currCarry = 0;
+        while (l1 != null || l2 != null) {
+            int a = 0, b = 0;
+            if (l1 != null) {
+                a = l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                b = l2.val;
+                l2 = l2.next;
+            }
+            int sum = a + b + currCarry;
+
+            currCarry = sum/10;
+            l3.next = new ListNode(sum%10);
+            l3 = l3.next;
+
+//        ListNode resultSum = reverseList(result.next);
+        }
+        if(currCarry>0) {
+            l3.next = new ListNode(currCarry);
+            l3 = l3.next;
+        }
+        return result.next;
+
+    }
+
+    private ListNode reverseList(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = reverseList(head.next);
+        ListNode headNext = head.next;
+        headNext.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
+
+
 public class AddNumbers {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(9);
+        l1.next.next = new ListNode(9);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        Solution solve = new Solution();
+        solve.addTwoNumbers(l1, l2);
+
+    }
 }
